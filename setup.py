857fa84ee9416c084dfcbbac72a2bdd940fe1d8e
Andie Shining Phan <asphan@gmail.com>
2020-05-21 17:49:50 -0700

    Add setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name="stream2py",
+    version="1.0.0",
+    author="Andie Shining Phan",
+    author_email="asphan@gmail.com",
+    description="Bring data streams to python, with ease.",
+    long_description="",
+    url="https://github.com/i2mint/stream2py",
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
