a401233643b1c286d0c1edbc37ca94feb2aeecba
twhalen <thor@otosense.com>
2020-07-15 15:37:26 -0700

    (trying to) figure out the input_device_index if None given Context manager and pure generator forms

--- stream2py/examples/usage/audio_pokes.py
+++ stream2py/examples/usage/audio_pokes.py
@@ -1,6 +1,7 @@
 from functools import partial
 from io import BytesIO
 from itertools import chain
+from contextlib import contextmanager
 
 from stream2py import StreamBuffer
 from stream2py.sources.audio import PyAudioSourceReader
@@ -27,7 +28,33 @@ def bytes_to_waveform(b, sr=DFLT_SR, n_channels=DFLT_N_CHANNELS, sample_width=DF
 
 ########################################################################################################################
 
+def find_a_device_index(filt='microphone', dflt=None):
+    if isinstance(filt, str):
+        match_str = filt
 
+        def filt(x):
+            return match_str in x.get('name', match_str).lower()
+    match = next(filter(filt, PyAudioSourceReader.list_device_info()), None)
+    return (match is not None and match['index']) or dflt
+
+
+def ensure_source_input_device_index(input_device_index=None):
+    input_device_index = input_device_index or find_a_device_index(filt='microphone', dflt=None)
+    if input_device_index is not None:
+        return input_device_index
+    else:
+        # TODO: Nicer way to print info (perhaps only relevant info, formated as table)
+        print("Need a valid input_device_index. Calling live_audio_chks.list_device_info() to information about the "
+              "devices I can detect:\n")
+        for item in PyAudioSourceReader.list_device_info():
+            print(item)
+            print("")
+        print("---> Look in the list above and choose an input_device_index (it's called index in the printout above) "
+              "that seems to be right for you!")
+        raise ValueError("Need a valid input_device_index")
+
+
+@contextmanager
 def live_audio_chks(input_device_index=None, sr=DFLT_SR, sample_width=DFLT_SAMPLE_WIDTH, n_channels=DFLT_N_CHANNELS,
                     chk_size=DFLT_CHK_SIZE, stream_buffer_size_s=DFLT_STREAM_BUF_SIZE_S):
     """A generator of live chunks of audio bytes taken from a stream sourced from specified microphone.
@@ -39,17 +66,7 @@ def live_audio_chks(input_device_index=None, sr=DFLT_SR, sample_width=DFLT_SAMPL
     :param sample_width: Specifies the number of frames per buffer.
     :param stream_buffer_size_s: How many seconds of data to keep in the buffer (i.e. how far in the past you can see)
     """
-
-    if input_device_index is None:  # TODO: Has no effect. See why.
-        # TODO: Nicer way to print info (perhaps only relevant info, formated as table)
-        print("Need a valid input_device_index. Calling live_audio_chks.list_device_info() to information about the "
-              "devices I can detect:\n")
-        for item in PyAudioSourceReader.list_device_info():
-            print(item)
-            print("")
-        print("---> Look in the list above and choose an input_device_index (it's called index in the printout above) "
-              "that seems to be right for you!")
-        raise ValueError("Need a valid input_device_index")
+    input_device_index = ensure_source_input_device_index(input_device_index)
 
     seconds_per_read = chk_size / sr
 
@@ -61,10 +78,46 @@ def live_audio_chks(input_device_index=None, sr=DFLT_SR, sample_width=DFLT_SAMPL
     _bytes_to_waveform = partial(bytes_to_waveform, sr=sr, n_channels=n_channels, sample_width=sample_width)
     with StreamBuffer(source_reader=source_reader, maxlen=maxlen) as stream_buffer:
         """keep open and save to file until stop event"""
-        yield from iter(stream_buffer)
+        yield iter(stream_buffer)
 
 
 live_audio_chks.list_device_info = PyAudioSourceReader.list_device_info
+
+
+# TODO: live_wf_ctx and live_wf: Lot's of repeated code. Address this.
+@contextmanager
+def live_wf_ctx(input_device_index=None, sr=DFLT_SR, sample_width=DFLT_SAMPLE_WIDTH, n_channels=DFLT_N_CHANNELS,
+                chk_size=DFLT_CHK_SIZE, stream_buffer_size_s=DFLT_STREAM_BUF_SIZE_S):
+    """A context manager providing a generator of live waveform sample values taken from a stream sourced
+    from specified microphone.
+
+    :param rate: Specifies the desired sample rate (in Hz)
+    :param sample_bytes: Sample width in bytes (1, 2, 3, or 4)
+    :param n_channels: The desired number of input channels. Ignored if input_device is not specified (or None).
+    :param input_device_index: Index of Input Device to use. Unspecified (or None) uses default device.
+    :param stream_buffer_size_s: How many seconds of data to keep in the buffer (i.e. how far in the past you can see)
+
+    >>> from time import sleep
+    >>> from itertools import islice
+    >>> # enter the id of your microphone and get a live waveform source!
+    >>> # (if None, will try to figure it out)
+    >>> with live_wf_ctx(input_device_index=None) as wf_gen:
+    ...
+    ...     # Now wait a bit, say some silly things, then ask for a few samples...
+    ...     sleep(1.1)
+    ...     wf = list(islice(wf_gen, 0, 44100 * 1))
+    >>> # and now listen to that wf and be embarrassed...
+    >>> # ... or just look at the size (less fun though)
+    >>> len(wf)
+    44100
+    """
+    with live_audio_chks(input_device_index=input_device_index,
+                         sr=sr, sample_width=sample_width, n_channels=n_channels,
+                         chk_size=chk_size,
+                         stream_buffer_size_s=stream_buffer_size_s) as live_audio_chunks:
+        _bytes_to_waveform = partial(bytes_to_waveform, sr=sr, n_channels=n_channels, sample_width=sample_width)
+        yield chain.from_iterable(map(lambda x: _bytes_to_waveform(x[1]), live_audio_chunks))
+    live_audio_chunks.close()
 
 
 def live_wf(input_device_index=None, sr=DFLT_SR, sample_width=DFLT_SAMPLE_WIDTH, n_channels=DFLT_N_CHANNELS,
@@ -77,22 +130,37 @@ def live_wf(input_device_index=None, sr=DFLT_SR, sample_width=DFLT_SAMPLE_WIDTH,
     :param input_device_index: Index of Input Device to use. Unspecified (or None) uses default device.
     :param stream_buffer_size_s: How many seconds of data to keep in the buffer (i.e. how far in the past you can see)
 
-    >>> # if you're not sure of the index of your microphone, use this to see a list
-    >>> device_info_list = live_wf.list_device_info()
-    >>> wf_gen = live_wf(input_device_index=1)  # enter the id of your microphone and get a live waveform source!
+    >>> from time import sleep
+    >>> from itertools import islice
+    >>> # enter the id of your microphone and get a live waveform source!
+    >>> # (if None, will try to figure it out)
+    >>> wf_gen = live_wf(input_device_index=None)
     >>>
     >>> # Now wait a bit, say some silly things, then ask for a few samples...
-    >>> from itertools import islice
-    >>> wf = list(islice(wf_gen, 0, 44100 * 3))
+    >>> sleep(1.2)
+    >>> wf = list(islice(wf_gen, 0, 44100 * 1))
     >>> # and now listen to that wf and be embarrassed...
-    >>>
+    >>> # ... or just look at the size (less fun though)
+    >>> len(wf)
+    44100
+
+    Don't forget to close! (or use live_wf_ctx context manager).
+    >>> wf_gen.close()
+
+    After wf_gen is closed, you can still ask it for data.
+    It just won't give you any.
+    >>> wf = list(islice(wf_gen, 0, 44100 * 1))
+    >>> len(wf)
+    0
+
+    Here wf_gen is a generator, so closing means: https://docs.python.org/2.5/whatsnew/pep-342.html
     """
     # TODO: Find a way to copy from containing function's signature and calling LiveAudioChunks with that
-    live_audio_chunks = live_audio_chks(
-        input_device_index=input_device_index, sr=sr, sample_width=sample_width, n_channels=n_channels,
-        chk_size=chk_size, stream_buffer_size_s=stream_buffer_size_s)
-    _bytes_to_waveform = partial(bytes_to_waveform, sr=sr, n_channels=n_channels, sample_width=sample_width)
-    yield from chain.from_iterable(map(lambda x: _bytes_to_waveform(x[1]), live_audio_chunks))
+    with live_wf_ctx(input_device_index=input_device_index,
+                     sr=sr, sample_width=sample_width, n_channels=n_channels,
+                     chk_size=chk_size,
+                     stream_buffer_size_s=stream_buffer_size_s) as live_wf:
+        yield from live_wf
 
 
 live_wf.list_device_info = PyAudioSourceReader.list_device_info
