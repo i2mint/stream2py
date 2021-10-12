d05de74c644460187430b7bc656052482086af73
Andie.Phan <Andie.Phan@analog.com>
2021-10-11 18:24:32 -0700

    update keyboard_and_audio.py with docs and comments

--- stream2py/examples/usage/keyboard_and_audio.py
+++ stream2py/examples/usage/keyboard_and_audio.py
@@ -1,16 +1,35 @@
-if __name__ == '__main__':
-    from stream2py.stream_buffer import StreamBuffer
-    from stream2py.sources.keyboard_input import KeyboardInputSourceReader
-    from stream2py.sources.audio import PyAudioSourceReader
+from stream2py.stream_buffer import StreamBuffer
+from stream2py.sources.keyboard_input import KeyboardInputSourceReader
+from stream2py.sources.audio import PyAudioSourceReader
 
-    # audio parameters
-    input_device_index = 2  # find index with PyAudioSourceReader.list_device_info()
-    rate = 44100
-    width = 2
-    channels = 1
-    frames_per_buffer = 44100  # same as sample rate for 1 second intervals
-    seconds_to_keep_in_stream_buffer = 60
 
+def keyboard_and_audio(
+    input_device_index=0,  # find index with PyAudioSourceReader.list_device_info()
+    rate=44100,
+    width=2,
+    channels=1,
+    frames_per_buffer=44100,  # same as sample rate for 1 second intervals
+    seconds_to_keep_in_stream_buffer=60,
+):
+    """Starts two independent streams: one for audio and another for keyboard inputs.
+    Prints stream type, timestamp, and additional info about data:
+    Shows input key pressed for keyboard and byte count for audio
+
+    Press Esc key to quit.
+
+    :param input_device_index: find index with PyAudioSourceReader.list_device_info()
+    :param rate: audio sample rate
+    :param width: audio byte width
+    :param channels: number of audio input channels
+    :param frames_per_buffer: audio samples per buffer
+    :param seconds_to_keep_in_stream_buffer: max size of audio buffer before data falls off
+    :return: None
+    """
+    selected_device_info = next(dev for dev in PyAudioSourceReader.list_device_info()
+                                if dev['index'] == input_device_index)
+    print(f"Starting audio device: {selected_device_info['name']}\n")
+
+    # converts seconds_to_keep_in_stream_buffer to max number of buffers of size frames_per_buffer
     maxlen = PyAudioSourceReader.audio_buffer_size_seconds_to_maxlen(
         buffer_size_seconds=seconds_to_keep_in_stream_buffer,
         rate=rate,
@@ -25,16 +44,19 @@ if __name__ == '__main__':
         frames_per_buffer=frames_per_buffer,
     )
 
+    # open audio stream
     with StreamBuffer(source_reader=audio_source_reader, maxlen=maxlen) as audio_stream_buffer:
+        # open keyboard input stream
         with StreamBuffer(source_reader=KeyboardInputSourceReader(), maxlen=maxlen) as keyboard_stream_buffer:
             audio_buffer_reader = audio_stream_buffer.mk_reader()
             keyboard_buffer_reader = keyboard_stream_buffer.mk_reader()
 
-            print('getch! Press any key! Esc to quit!')
+            print('getch! Press any key! Esc to quit!\n')
             while True:
                 keyboard_data = next(keyboard_buffer_reader)
                 audio_data = next(audio_buffer_reader)
 
+                # do stuff with keyboard data
                 if keyboard_data is not None:
                     index, keyboard_timestamp, char = keyboard_data
                     print(f'[Keyboard] {keyboard_timestamp}: {char}', end='\n\r')
@@ -42,6 +64,11 @@ if __name__ == '__main__':
                     if char == '\x1b':  # ESC key
                         break
 
+                # do stuff with audio data
                 if audio_data is not None:
                     audio_timestamp, waveform, frame_count, time_info, status_flags = audio_data
-                    print(f'   [Audio] {audio_timestamp}:  {len(waveform)=} {type(waveform).__name__}', end='\n\r')
+                    print(f'   [Audio] {audio_timestamp}: {len(waveform)=} {type(waveform).__name__}', end='\n\r')
+
+
+if __name__ == '__main__':
+    keyboard_and_audio(input_device_index=0)
