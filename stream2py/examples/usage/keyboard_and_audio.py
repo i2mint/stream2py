1f2bf2a2090852792e04031df78e8fa97117e595
GitHub CI Runner <vferon@pentalog.com>
2021-10-12 01:27:53 +0000

    **CI** Formatted code + Updated version number and documentation. [skip ci]

--- stream2py/examples/usage/keyboard_and_audio.py
+++ stream2py/examples/usage/keyboard_and_audio.py
@@ -25,15 +25,18 @@ def keyboard_and_audio(
     :param seconds_to_keep_in_stream_buffer: max size of audio buffer before data falls off
     :return: None
     """
-    selected_device_info = next(dev for dev in PyAudioSourceReader.list_device_info()
-                                if dev['index'] == input_device_index)
+    selected_device_info = next(
+        dev
+        for dev in PyAudioSourceReader.list_device_info()
+        if dev['index'] == input_device_index
+    )
     print(f"Starting audio device: {selected_device_info['name']}\n")
 
     # converts seconds_to_keep_in_stream_buffer to max number of buffers of size frames_per_buffer
     maxlen = PyAudioSourceReader.audio_buffer_size_seconds_to_maxlen(
         buffer_size_seconds=seconds_to_keep_in_stream_buffer,
         rate=rate,
-        frames_per_buffer=frames_per_buffer
+        frames_per_buffer=frames_per_buffer,
     )
     audio_source_reader = PyAudioSourceReader(
         rate=rate,
@@ -45,9 +48,13 @@ def keyboard_and_audio(
     )
 
     # open audio stream
-    with StreamBuffer(source_reader=audio_source_reader, maxlen=maxlen) as audio_stream_buffer:
+    with StreamBuffer(
+        source_reader=audio_source_reader, maxlen=maxlen
+    ) as audio_stream_buffer:
         # open keyboard input stream
-        with StreamBuffer(source_reader=KeyboardInputSourceReader(), maxlen=maxlen) as keyboard_stream_buffer:
+        with StreamBuffer(
+            source_reader=KeyboardInputSourceReader(), maxlen=maxlen
+        ) as keyboard_stream_buffer:
             audio_buffer_reader = audio_stream_buffer.mk_reader()
             keyboard_buffer_reader = keyboard_stream_buffer.mk_reader()
 
@@ -66,8 +73,17 @@ def keyboard_and_audio(
 
                 # do stuff with audio data
                 if audio_data is not None:
-                    audio_timestamp, waveform, frame_count, time_info, status_flags = audio_data
-                    print(f'   [Audio] {audio_timestamp}: {len(waveform)=} {type(waveform).__name__}', end='\n\r')
+                    (
+                        audio_timestamp,
+                        waveform,
+                        frame_count,
+                        time_info,
+                        status_flags,
+                    ) = audio_data
+                    print(
+                        f'   [Audio] {audio_timestamp}: {len(waveform)=} {type(waveform).__name__}',
+                        end='\n\r',
+                    )
 
 
 if __name__ == '__main__':
